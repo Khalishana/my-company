5493873e3eed171b6528f8bf2f2f7a6d3557339e
khalishana <khalisha.hana10@gmail.com>
2024-06-12 10:01:32 +0700

    feat: add base templates

--- hana_kuliner/settings.py
+++ hana_kuliner/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hana_kuliner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], #tambah berkas base.html
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from main.forms import ProductForm
+from django.urls import reverse
 
 # Create your views here.
 def show_main(request):
